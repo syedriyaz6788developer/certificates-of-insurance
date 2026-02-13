5e778857ecfa8ade42c9ae94ebc826f6ae45196e
anupam-react <react6@flyweis.technology>
2026-02-13 19:34:29 +0530

    new changes updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,6 @@ function IconButton({ icon, children, className = "", ...props }) {
   );
 }
 
-
 export default function Header({ setIsOpen, isMobileView, isTabletView }) {
   const [isBulkReminderOpen, setIsBulkReminderOpen] = useState(false);
   const [isProfileOpen, setIsProfileOpen] = useState(false);
